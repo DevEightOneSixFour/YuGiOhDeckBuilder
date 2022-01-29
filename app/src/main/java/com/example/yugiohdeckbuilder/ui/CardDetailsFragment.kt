27ac3d72a137635136ccdead24d7e49aaf280f02
DevEightOneSixFour <sebrown@enhanceit.com>
2022-01-29 14:22:53 -0500

    Details Fragment

--- app/src/main/java/com/example/yugiohdeckbuilder/ui/CardDetailsFragment.kt
+++ app/src/main/java/com/example/yugiohdeckbuilder/ui/CardDetailsFragment.kt
@@ -1,6 +1,31 @@
 package com.example.yugiohdeckbuilder.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.yugiohdeckbuilder.databinding.FragmentDetailsBinding
 
 class CardDetailsFragment: Fragment() {
+
+    private val binding by lazy { FragmentDetailsBinding.inflate(layoutInflater) }
+    private val args : CardDetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            Glide.with(ivLargeCard)
+                .load(args.card.cardImages[0].imageUrl)
+                .into(ivLargeCard)
+        }
+    }
 }
